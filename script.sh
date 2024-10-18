61982782ba08e43cdeaa55f46d4a2c4ad75f11ef
Camille De Amorim <camille.deamorim17@gmail.com>
2024-10-18 13:43:46 +0200

    recette featureCounts et ajout dans script bash

--- script.sh
+++ script.sh
@@ -13,3 +13,8 @@ apptainer exec container/bowtie.img bowtie-build workflow_execution/data/referen
 if [ ! -d workflow_execution/mapping ] ; then mkdir workflow_execution/mapping ; fi
 apptainer run container/bowtie.img -p 8 -S workflow_execution/data/genome_index workflow_execution/trimmed/PRJNA586837_trimmed.fq | apptainer run container/samtools.img sort -@ 8 > workflow_execution/mapping/PRJNA586837.bam
 apptainer run container/samtools.img index workflow_execution/mapping/PRJNA586837.bam
+
+wget -O workflow_execution/data/reference.gff "https://www.ncbi.nlm.nih.gov/sviewer/viewer.cgi?db=nuccore&report=gff3&id=CP000253.1"
+
+mkdir -p workflow_execution/featureCounts
+apptainer run container/featurecounts.img --extraAttributes Name -t gene -g ID -F GTF -T 8 -a workflow_execution/data/reference.gff -o workflow_execution/featureCounts/counts.txt workflow_execution/mapping/PRJNA586837.bam
