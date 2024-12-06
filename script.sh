996241113d35f2b2d3aa9c9e7a641c3f8811e1ec
Georgeshanna2023 <georges.hanna.info@gmail.com>
2024-12-06 17:51:09 +0100

    remove sh script

--- script.sh
+++
@@ -1,8 +0,0 @@
-
-apptainer build images/fasterq-dump.img container/Singularity.fasterq-dump
-apptainer build images/TrimGalor.img container/Singularity.TrimGalor
-apptainer build images/bowtie_new_version.img container/Singularity.bowtie_v1.3.1
-apptainer build images/samtools_new_version.img container/Singularity.samtools_v1.21
-apptainer build images/featureCounts_new_version.img container/Singularity.featureCounts_v2.0.7
-
-
